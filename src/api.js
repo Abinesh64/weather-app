89e1eb35c7069258737cfa6b167dcd5d8046f7f5
Abinesh <abineshrajendran64@gmail.com>
2023-08-11 22:29:06 +0530

    Commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '68d3650240msh6fe196f2c79ed3cp16b7bejsn1c7fbdfd0b4b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "43a5d8cccc4218f3a0a4443898518104"
